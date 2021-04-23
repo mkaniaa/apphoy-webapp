e1d0a03168bb0946fb95972d9452edca0ddf5c0e
mkaniaa <mkaniaa@gmail.com>
2021-04-23 22:44:18 +0200

    Edit participant form.

--- apphoy/participants/views.py
+++ apphoy/participants/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.forms.widgets import DateInput
+from django.views.generic.edit import FormMixin
 
 from .models import Participant
 
@@ -24,13 +26,22 @@ class ManageParticipantMixin(LoginRequiredMixin, PermissionRequiredMixin):
     success_url = reverse_lazy('participant_list')
 
 
+class ParticipantFormMixin(FormMixin):
+    def get_form(self, form_class=None):
+        form = super(ParticipantFormMixin, self).get_form()
+        form.fields['birth_date'].widget = DateInput()
+        return form
+
+
 class ParticipantAddView(ManageParticipantMixin,
+                         ParticipantFormMixin,
                          CreateView):
     template_name = 'participants/manage/participant/form.html'
     permission_required = 'participant.add_participant'
 
 
 class ParticipantEditView(ManageParticipantMixin,
+                          ParticipantFormMixin,
                           UpdateView):
     template_name = 'participants/manage/participant/form.html'
     permission_required = 'participant.change_participant'
