b1727349bb1434055afc4a911d79d8cb7a0eaac4
mkaniaa <mkaniaa@gmail.com>
2021-04-28 22:41:05 +0200

    Adding/changing participant form finished.

--- apphoy/participants/views.py
+++ apphoy/participants/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
 
 from .models import Participant
 
