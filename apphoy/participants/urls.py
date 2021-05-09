31b6e5f1b796af942715d3c6b9c51b378a3be684
mkaniaa <mkaniaa@gmail.com>
2021-05-09 19:21:20 +0200

    Removing multiple checked participants - in progress.

--- apphoy/participants/urls.py
+++ apphoy/participants/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('add/', views.ParticipantAddView.as_view(), name='participant_add'),
     path('<pk>/edit/', views.ParticipantEditView.as_view(), name='participant_edit'),
-    path('<pk>/delete/', views.ParticipantDeleteView.as_view(), name='participant_delete'),
+    path('<int:participant_id>/delete/', views.ParticipantDeleteView.as_view(), name='participant_delete'),
 ]
