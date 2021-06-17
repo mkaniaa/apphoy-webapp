e787605a1d09fd299d1d5937a665265774829c52
mkaniaa <mkaniaa@gmail.com>
2021-06-17 22:37:25 +0200

    Adding trip - init.

--- apphoy/trips/urls.py
+++ apphoy/trips/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TripStageListView.as_view(), name='trip_stages_list'),
+    path('', views.TripStageListView.as_view(), name='trip_list'),
+    path('add/', views.TripAddView.as_view(), name='trip_add'),
+    path('<pk>/edit/', views.TripEditView.as_view(), name='trip_edit'),
+    path('<int:trip_id>/delete/', views.TripDeleteView.as_view(), name='trip_delete'),
 ]
--- apphoy/trips/views.py
+++ apphoy/trips/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
-from django.views.generic.base import TemplateResponseMixin, View
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, UpdateView
+from django.views.generic.base import TemplateResponseMixin
 
+from apphoy.models import get_field_names
 from .models import Trip, TripStage
 
 
 class TripStageListView(TemplateResponseMixin, View):
     model = TripStage
-    template_name = 'trips/trip_stages/list.html'
+    template_name = 'trips/list.html'
 
     def get(self, request, trip_slug=None):
         if trip_slug is None:
@@ -26,3 +31,26 @@ class TripStageListView(TemplateResponseMixin, View):
         return self.render_to_response({'trips': trips,
                                         'trip': trip,
                                         'trip_stages': trip_stages})
+
+
+class ManageTripMixin(LoginRequiredMixin, PermissionRequiredMixin):
+    model = Trip
+    fields = get_field_names(model, exclude=['id'])
+    success_url = reverse_lazy('trip_list')
+
+
+class TripAddView(ManageTripMixin, CreateView):
+    template_name = 'trips/manage/trip/form.html'
+    permission_required = 'trip.add_trip'
+
+
+class TripEditView(ManageTripMixin, UpdateView):
+    template_name = 'trips/manage/trip/form.html'
+    permission_required = 'trip.change_trip'
+
+
+class TripDeleteView(View):
+    def post(self, request, trip_id):
+        trip = get_object_or_404(Trip, id=trip_id)
+        trip.delete()
+        return redirect('trip_list')
