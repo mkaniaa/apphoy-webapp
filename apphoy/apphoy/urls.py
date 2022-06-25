40a7d5dcc39e344645ffcd4e42f35038e5769f85
mkaniaa <mkaniaa@gmail.com>
2022-06-25 16:23:07 +0200

    AP-5 Stylize logging page
    * Stylizing logging and logout pages.

--- apphoy/apphoy/urls.py
+++ apphoy/apphoy/urls.py
@@ -5,13 +5,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name="home")
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name="home")
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -20,11 +20,15 @@ from django.urls import path, include
 from common.views import redirect_view
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/profile/', redirect_view, name='profile_main'),
-    path('admin/', admin.site.urls),
-    path('', redirect_view, name='main'),
-    path('persons/', include('persons.urls')),
-    path('trips/', include('trips.urls')),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(template_name='registration/logout.html'),
+        name="logout",
+    ),
+    path("accounts/profile/", redirect_view, name="profile_main"),
+    path("admin/", admin.site.urls),
+    path('', redirect_view, name="main"),
+    path("persons/", include("persons.urls")),
+    path("trips/", include("trips.urls")),
 ]
